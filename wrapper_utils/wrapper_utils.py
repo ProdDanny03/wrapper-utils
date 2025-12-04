b49245bbd1bc018ecdb475dced7f449dc3d5e930
ProdDanny03 <proddanny03@gmail.com>
2025-12-04 12:06:29 +0000

    added docstrings

--- wrapper_utils/wrapper_utils.py
+++ wrapper_utils/wrapper_utils.py
@@ -7,6 +7,17 @@ from concurrent.futures import ThreadPoolExecutor
 _DEFAULT_POOL = ThreadPoolExecutor()
 
 def repeat(n=1):
+    """
+    The `repeat` function is a Python decorator that allows a function to be executed multiple times
+    based on the specified number `n`.
+    @param n () - The `n` parameter in the `repeat` function is a default parameter with a default value
+    of 1. This parameter is used to specify the number of times a decorated function should be repeated
+    when it is called. If `n` is not provided when calling the `repeat` decorator, it
+    @returns The `repeat` function is returning a decorator function `decorator_repeat`. This decorator
+    function takes another function `func` as an argument and returns a wrapped function
+    `wrapper_repeat` that will repeat the execution of `func` `n` times.
+    Danny - 12/04/2025
+    """
     def decorator_repeat(func):
         @functools.wraps(func)
         def wrapper_repeat(*args, **kwargs):
@@ -17,6 +28,19 @@ def repeat(n=1):
     return decorator_repeat
 
 def threaded_repeat(n=1, executor=None):
+    """
+    The `threaded_repeat` function is a decorator in Python that allows a specified function to be
+    executed repeatedly in a threaded manner using a specified executor.
+    @param n () - The `n` parameter in the `threaded_repeat` function specifies the number of times a
+    given function should be repeated in a threaded manner. By default, if no value is provided for `n`,
+    it will be set to 1, meaning the function will be executed once.
+    @param executor () - The `executor` parameter in the `threaded_repeat` function is used to specify
+    the executor (thread pool or process pool) that will be used to submit the repeated function calls
+    for execution. If no executor is provided, it defaults to `_DEFAULT_POOL`. This allows you to
+    control the execution environment
+    @returns The `threaded_repeat` function returns a decorator function `decorator_threaded_repeat`.
+    Danny - 12/04/2025
+    """
     def decorator_threaded_repeat(func):
         @functools.wraps(func)
         def wrapper_threaded_repeat(*args, **kwargs):
@@ -28,10 +52,29 @@ def threaded_repeat(n=1, executor=None):
         return wrapper_threaded_repeat
     return decorator_threaded_repeat
 
-def silent_catch(_func=None, *, exception=None):
-    return catch(_func=_func, exception=exception, silent=True)
-
 def catch(_func=None, *, exception=None, handler=None, silent=False):
+    """
+    The `catch` function is a decorator in Python that allows for catching specified exceptions and
+    handling them with optional custom handlers.
+    @param _func () - The `_func` parameter is a function that can be passed as an argument to the
+    `catch` decorator. It is used to specify the function that you want to wrap with the error handling
+    logic provided by the `catch` decorator.
+    @param exception () - The `exception` parameter in the `catch` function is used to specify the type
+    of exception that should be caught. If no specific exception type is provided, it defaults to
+    catching all exceptions of type `Exception`. If a list of exception types is provided, they are
+    converted to a tuple for handling
+    @param handler () - The `handler` parameter in the `catch` function is used to specify a custom
+    function that will be called when an exception is caught. If a `handler` function is provided, it
+    will be called with the exception object as its argument instead of printing the traceback. This
+    allows you to define your
+    @param silent () - The `silent` parameter in the `catch` function is a boolean flag that determines
+    whether the exception should be handled silently without printing any traceback information. If
+    `silent` is set to `True`, the exception will be caught and handled without any output to the
+    console. If `silent` is `
+    @returns The `catch` function is returning a decorator function called `decorator_catch` if `_func`
+    is None, or it is returning the result of calling `decorator_catch` with `_func` as an argument.
+    Danny - 12/04/2025
+    """
     if not exception:
         exception = Exception
     if type(exception) is list:
@@ -55,6 +98,24 @@ def catch(_func=None, *, exception=None, handler=None, silent=False):
             return decorator_catch(_func)
 
 def timeit(_func=None, *, timer=time.perf_counter, handler=None):
+    """
+    The `timeit` function is a decorator in Python that measures the execution time of a function and
+    optionally calls a handler function with the timing information.
+    @param _func () - The `_func` parameter in the `timeit` function is used to optionally pass a
+    function to be timed. If `_func` is provided, the decorator `decorator_timeit` is applied directly
+    to that function. If `_func` is not provided (i.e., it is `None
+    @param timer () - The `timer` parameter in the `timeit` function is used to specify the timer
+    function that will be used to measure the execution time of the decorated function. By default, it
+    is set to `time.perf_counter`, which is a high-resolution timer function in the `time` module of
+    @param handler () - The `handler` parameter in the `timeit` function is used to specify a callback
+    function that will be called after the execution of the decorated function. This callback function
+    can be used to perform custom actions with the timing information of the function execution, such as
+    logging the timing data to a file,
+    @returns The `timeit` function returns either the `decorator_timeit` function or the result of
+    calling `decorator_timeit` with the provided function `_func`, depending on whether `_func` is
+    `None` or not.
+    Danny - 12/04/2025
+    """
     def decorator_timeit(func):
         @functools.wraps(func)
         def wrapper_timeit(*args, **kwargs):
